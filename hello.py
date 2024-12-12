3bb23c7fe235ebd300d89c5f3d3549444a9de052
sarthak576 <sarthakguptarjn@gmail.com>
2024-12-13 04:23:42 +0530

    really fun to code

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ Base = declarative_base()
 
 ## step 2 : create simple table 
 
-class student(Base):
+class Student(Base):
     __tablename__ = "SG-Ki-Table"
     id = Column(Integer,primary_key=True)
     name = Column(String(12),nullable=False)
@@ -16,5 +16,17 @@ class student(Base):
 
 ## step 3 : Connect to SQL Lite Database 
 
-engine = create_engine("sqlite:///student.db")
+engine = create_engine("sqlite:///student.db") ## create a sqllite Database 
 Base.metadata.create_all(engine) # creates tabel in the database
+
+## step 4 : Create a session 
+Session = sessionmaker(bind=engine)
+session = Session() 
+
+## step 5 : Add a student record 
+new_student = Student(name="John-Wick", age=41) # create a new student (row)
+session.add(new_student) # add student to session
+session.commit()
+
+print("\nstudent added successfully :-}") 
+print("What a pain in ASS!")
